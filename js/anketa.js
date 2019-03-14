ca887683311ea173c5b12f5268c3868c4ef09d64
luisjanj <luisjanj@gmail.com>
2019-03-14 20:22:52 +0100

    html

--- js/anketa.js
+++ js/anketa.js
@@ -1,29 +1,17 @@
-function provera()
-{
-var pitanje1 = document.getElementById('pitanje1').value;
-var pitanje2 = document.getElementById('pitanje2').value;
-var pitanje3 = document.getElementById('pitanje3').value;
-var pitanje4 = document.getElementById('pitanje4').value;
-var pitanje5 = document.getElementById('pitanje5').value;
-localStorage.setItem("Da li ste zainteresovani za putovanje na mars?", pitanje1);
-localStorage.setItem("Kolika je udaljenost zemlje od marsa?", pitanje2);
-localStorage.setItem("Koliko novca ste spremni da izdvojite za putovanje?", pitanje3);
-localStorage.setItem("Da li postoji voda na marsu?", pitanje4);
-localStorage.setItem("Da li smatrate da ima zivota na marsu?", pitanje5);
-return  uneto_pitanje1();   
+var list = document.getElementById('pitanja');
+    
+    function dodajPitanja(){
+    var pitanje1 = document.getElementById('pitanje1').value;
+    var pitanje2 = document.getElementById('pitanje2').value;
+    var pitanje3 = document.getElementById('pitanje3').value;
+    var pitanje4 = document.getElementById('pitanje4').value;
+    var pitanje5 = document.getElementById('pitanje5').value;
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(pitanje1 + ' ' + pitanje2 + ' ' + pitanje3 + ' ' + pitanje4 + ' ' + pitanje5));
+    list.appendChild(entry);
+    
+
+    return false;
 }
 
-function uneto_pitanje1()
-{
-var pitanje1 = document.getElementById('pitanje1').value;
-var pitanje2 = document.getElementById('pitanje2').value;
-var pitanje3 = document.getElementById('pitanje3').value;
-var pitanje4 = document.getElementById('pitanje4').value;
-var pitanje5 = document.getElementById('pitanje5').value;
-if(pitanje1.trim().length == 0 || pitanje2.trim().length == 0 || pitanje3.trim().length == 0 || pitanje4.trim().length == 0 || pitanje5.trim().length == 0)
-{
-greska.innerHTML = "Unesite odgovor";
-return false;
-}
-return true;  
-}
+
